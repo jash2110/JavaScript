949e33824160d0afc0faca8a9167a7805eff396e
jash2110 <118957515+jash2110@users.noreply.github.com>
2025-04-29 18:15:33 +0000

    datatypes

---
+++ 01_basics/04_compareDT.js
@@ -0,0 +1,19 @@
+console.log(2==2);
+console.log(2>=2);
+console.log(2<=2);
+console.log(2<2);
+console.log("2" == 2)
+console.log("2" === 2)
+
+console.log(null == 0)
+console.log(null > 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined >= 0)
+console.log(undefined <= 0);
+console.log(undefined === 0) //error
+
+console.log(null == null);
+console.log(null >= null);
+console.log(null > null);
---
+++ 01_basics/05_datatypes.js
@@ -0,0 +1,19 @@
+const number = 2000
+
+const myFunction = function(){
+    console.log("Hello World");
+    
+}
+//console.log(myFunction());
+myFunction();
+
+const array = ["car", "bus", "river"]
+console.log(array);
+
+console.table(array);
+
+let myObj = {
+    name : "jash", 
+    age : 20
+}
+console.log(myObj);
