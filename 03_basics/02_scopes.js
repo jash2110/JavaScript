11de5f5e9c2d89889b3a43c7e7ae77b24b11ca55
jash2110 <118957515+jash2110@users.noreply.github.com>
2025-06-19 06:42:40 +0000

    scopes updated

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -6,10 +6,26 @@ if(true){
     let a = 10
     const b = 20
     var c = 3
-    console.log("Inner A: ",a);
-    console.log("Inner B: ",b);
+    // console.log("Inner A: ",a);
+    // console.log("Inner B: ",b);
 }
 
-console.log("Outer A:",a);
-console.log("Outer B:",b);
-console.log(c);
+// console.log("Outer A:",a);
+// console.log("Outer B:",b);
+// console.log(c);
+
+
+
+function one(){
+    const username = "JVM"
+
+    function two(){
+        const name = " jash"
+        console.log(username + name);
+    }
+    // console.log(name);
+    two()
+    console.log(username);
+}
+
+one()
