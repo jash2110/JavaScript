030fd31ce02379df721545eb6977e9aa8c548881
jash2110 <118957515+jash2110@users.noreply.github.com>
2025-06-19 06:07:24 +0000

    scopes

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -4,7 +4,7 @@ function addNums(num1, num2) {
 }
 
 const res = addNums(5, 10)
-// console.log("Result:", res);
+console.log("Result:", res);
 
 function logIn(name){
 
@@ -23,7 +23,7 @@ function cartItems(...number){
     return number
 }
 
-// console.log(cartItems(500, 200, 100))
+console.log(cartItems(500, 200, 100))
 
 const details = {
     name: "jash",
@@ -34,7 +34,7 @@ function userDetails(obj){
     console.log(`Name: ${obj.name}, Price: ${obj.price}`);
 }
 
-// userDetails(details);
+userDetails(details);
 
 userDetails({
     name: "jash",
@@ -47,7 +47,7 @@ function getElement(myArray) {
     return myArray[2];
 }
 
-// console.log(getElement(array));
+console.log(getElement(array));
 
 
 console.log(getElement([1, 2, 3, 4, 5]));
---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,15 @@
+let a = 1000
+const b = 200
+var c = 30
+
+if(true){
+    let a = 10
+    const b = 20
+    var c = 3
+    console.log("Inner A: ",a);
+    console.log("Inner B: ",b);
+}
+
+console.log("Outer A:",a);
+console.log("Outer B:",b);
+console.log(c);
