c3e05245f606e90020073548ca2eac890ff26a3a
jash2110 <118957515+jash2110@users.noreply.github.com>
2025-06-23 07:14:48 +0000

    while - do - while

---
+++ 05_iterations/two.js
@@ -0,0 +1,22 @@
+let index = 1
+
+// while (index <= 10) {
+//     console.log(`value of index is ${index}`);
+//     index = index + 2
+// }
+
+let myArray = ["flash", "superman", "batman", "wonderwoman"];
+
+let arr = 0
+// while (arr < myArray.length){
+//     console.log(`value is ${myArray[arr]}`);
+//     arr++;
+// }
+
+
+let score = 1 
+
+do {
+    console.log(`score is ${score}`);
+    score++
+} while (score <= 10);
